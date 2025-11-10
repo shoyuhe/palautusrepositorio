021ee948900e6f780a6490156fd555a460c1e27a
shoyuhe <sofia.heikkinen@icloud.com>
2025-11-10 21:05:35 +0200

    Add osamaara.py

---
+++ viikko2/git-branch-harjoitus/osamaara.py
@@ -0,0 +1,16 @@
+from logger import logger
+from summa import summa
+from erotus import erotus
+from tulo import tulo
+from osamaara import osamaara
+
+logger("aloitetaan ohjelma")
+
+x = int(input("luku 1: "))
+y = int(input("luku 2: "))
+print(f"{x} + {y} = {summa(x, y)}") 
+print(f"{x} - {y} = {erotus(x, y)}") 
+print(f"{x} / {y} = {osamaara(x, y)}") 
+
+logger("lopetetaan")
+print("goodbye!")
