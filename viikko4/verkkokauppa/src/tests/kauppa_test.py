a68225b585d36fd79c28d7d564e8497345213086
shoyuhe <sofia.heikkinen@icloud.com>
2025-12-01 16:01:37 +0200

    Adjust file

--- viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -91,7 +91,7 @@ class TestKauppa(unittest.TestCase):
         self.kauppa.tilimaksu("pekka", "12345")
 
         # varmistetaan, että arvot ovat oikeat
-        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 5) 
+        self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", "33333-44455", 5)
 
         #nollaus
         self.kauppa.aloita_asiointi()
